e0288f1f62172ef057a2b140aa1c7f0587488451
ambeyasuo-hash <ambeyasuo@gmail.com>
2026-01-19 19:09:55 +0900

    fix: install missing autoprefixer and postcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // src配下すべてを対象にする
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
